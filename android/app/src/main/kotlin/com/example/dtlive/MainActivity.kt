8ee15a5dc887eff8d4d6796554a2edeed9c3fcb9
AzureAD\HariChandu <hari.m@devpointsolutions.com>
2025-11-22 13:23:46 -0500

    delete the android and ios folders, its a website code.

--- android/app/src/main/kotlin/com/example/dtlive/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.media9newsnetwork.media9
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterActivity() {
-}
